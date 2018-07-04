d122ab361025734fe9760a8562b01f969b54c051
Daniel García Jones <danielgj@gmail.com>
2018-07-04 09:45:40 +0200

    Added config.js with connection values :(

---
+++ api/config/config.js
@@ -0,0 +1,10 @@
+module.exports = function() { 
+    
+    var config = {};
+    
+    config.base_api_v1_url = '/';
+    config.db_url = 'mongodb://dbUser:Unisys01@ds125331.mlab.com:25331/url-shorter-db';
+    
+    return config;   
+    
+}
