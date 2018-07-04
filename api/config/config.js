0ff0d60660bfc747d393b25c7a2b4608f9197014
Daniel García Jones <danielgj@gmail.com>
2018-07-04 09:40:35 +0200

    Added config file with database connection URL

---
+++ api/config/config.js
@@ -0,0 +1,10 @@
+module.exports = function() { 
+    
+    var config = {};
+    
+    config.base_api_v1_url = '/';
+    config.db_url = 'mongodb://dbUser:dbPass@dbURL';
+    
+    return config;   
+    
+}
