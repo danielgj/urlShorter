da62ef6ac0b85488bad1a8cf731861d513ac1a20
Daniel García Jones <danielgj@gmail.com>
2018-07-04 15:45:25 +0200

    Added header and footer formats

--- web/src/_components/Footer.js
+++ web/src/_components/Footer.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
+import dgjIcon from '../img/dgj.png';
+import twitterIcon from '../img/twitter.png';
+import linkedinIcon from '../img/linkedin.png';
+import githubIcon from '../img/github.png';
+
 export class Footer extends Component {
   render() {
     return (
-      <p>Footer</p>
+      <footer className="fixed-bottom navbar bg-dark">
+               <img src={dgjIcon} alt="Daniel Garcia Jones" title="Daniel Garcia Jones" className="rounded-circle footerImg"/>
+                <div className="row">
+                        <div className="col"></div>
+                </div>        
+                <div className="row">
+                    <div className="col">
+                        <p className="footerCopy text-center">&copy; 2018 Daniel García Jones</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <p className="text-right footerContact">
+                            <a href="https://github.com/danielgj/urlShorter" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="github_profile" title="github_profile" className="socialIcon"/></a>
+                            <a href="https://www.linkedin.com/in/dgarciaj/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="linkedin_profile" title="linkedin_profile" className="socialIcon"/></a>
+                            <a href="https://twitter.com/danielgarjones/" target="_blank" rel="noopener noreferrer"><img src={twitterIcon} alt="twitter_profile" title="twitter_profile" className="socialIcon"/></a>
+                        </p>
+                    </div>
+                </div>       
+      </footer>
     );
   }
 }
--- web/src/_components/Header.js
+++ web/src/_components/Header.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export class Header extends Component {
   render() {
     return (
-      <p>Header</p>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+          <a className="navbar-brand" href="/">URLShorter</a>          
+        </nav>                                                    
+      </header>
     );
   }
 }
