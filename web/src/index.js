51cece40c8ed5e5531ef2abd61b090bd221301b4
Daniel García Jones <danielgj@gmail.com>
2018-07-04 15:21:06 +0200

    Added redux structure and dependencies

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
+import { Provider } from 'react-redux';
+import { store } from './_helpers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './css/index.css';
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+
 registerServiceWorker();
