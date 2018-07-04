f062771fd5ee8d89ccc58bfc88db30e0779395ee
Daniel García Jones <danielgj@gmail.com>
2018-07-04 09:48:01 +0200

    Added Url model

---
+++ api/models.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+module.exports = function(wagner, config) {
+
+  mongoose.Promise = global.Promise;
+  mongoose.connect(config.db_url);
+
+  var Url =
+      mongoose.model('Url', require('./schemas/url'), 'urls');
+  
+  var models = {
+    Url: Url
+  }
+  
+   // To ensure DRY-ness, register factories in a loop
+  _.each(models, function(value, key) {
+    wagner.factory(key, function() {
+      return value;
+    });
+  });
+
+  return models;
+};
